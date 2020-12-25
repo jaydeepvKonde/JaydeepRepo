4c0ebe6ed922b7dc9df9e913603e5726f1934f0e
jaydeepvKonde <76567195+jaydeepvKonde@users.noreply.github.com>
2020-12-25 20:22:00 +0530

    Initial Commit

---
+++ precache-manifest.c48d246a7918f2c90d0331ae5c9efc2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50f54504c97f2d5340a98a42c8e065f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f4751d4ada29757a5b7",
+    "url": "/static/css/2.48f626b4.chunk.css"
+  },
+  {
+    "revision": "14249a6d5cfb35f0bcf5",
+    "url": "/static/css/main.3698e861.chunk.css"
+  },
+  {
+    "revision": "2f4751d4ada29757a5b7",
+    "url": "/static/js/2.1b72c8c4.chunk.js"
+  },
+  {
+    "revision": "90c012616547f23c5e1308b94b4ef79c",
+    "url": "/static/js/2.1b72c8c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14249a6d5cfb35f0bcf5",
+    "url": "/static/js/main.6d193ee1.chunk.js"
+  },
+  {
+    "revision": "89e5c496f7d697581e6d",
+    "url": "/static/js/runtime-main.013c9da6.js"
+  }
+]);
